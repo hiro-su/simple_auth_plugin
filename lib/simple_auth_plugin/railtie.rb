ae468b50a13c4cf2f94f747ec8cec0d78934c54d
hiro-su <h.sugipon@gmail.com>
2014-12-17 00:49:00 +0900

    typo

--- lib/simple_auth_plugin/railtie.rb
+++ lib/simple_auth_plugin/railtie.rb
@@ -10,7 +10,7 @@ module SimpleAuthPlugin
       puts "SimpleAuthPlugin is loaded!"
       require 'simple_auth_plugin/authable'
       ActiveSupport.on_load(:action_controller) do
-        ActionController::Baes.send :include, Authable
+        ActionController::Base.send :include, Authable
       end
     end
 
